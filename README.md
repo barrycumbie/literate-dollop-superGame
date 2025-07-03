c6ed35e39b18777a78156d3ff9d2164f9578b07a
Cumbie <bcumbie@CSM2-0244-MFXXH.main.local.una.edu>
2025-07-03 11:00:14 -0500

    almost there

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # literate-dollop-superGame
 CIS 376 Summer 2025 Block 02 Project 
+
+
+Task List
+
+1. Add in DOM stuff
+  1. buttons: reset, play (https://getbootstrap.com/docs/5.0/components/button-group/)
+  1. greeting
+    1. https://getbootstrap.com/docs/5.0/forms/input-group/
+    1. (stole this but removed the `<form` containter, don't need without a server>)
+  1. play 
+1. Add in user input, hello user (try catch)
+1. Clean up footer
+  1. source code link
+1. CSS: 
+  1. @import for bs5 icons & a Google font
+  1. using the Google font in a css rule
+  1. an advanced css selector
+  1. another css rule
+1. js
+  1. doc ready
+  1. welcome/greet
+  1. game events (win/lose/score)
+  1. array 
+  1. easter egg
+  1. user data, array of objects 
+1. refator dev profile 
--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -1,9 +1,42 @@
-// todo: add in doc read js f/n
+// global variables
+let playerName, winningHouse = ''; 
+let playerScore = 0; 
 
-console.log("Game script loaded");
 
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Game script loaded');
+  
+  // 1️⃣ Initialize the game, hide the game pieces, pick a winning house
+  initializeGame();   
+    
+  //  
+  document.getElementById("submitPlayerName").addEventListener("click", ()=>{greetPlayer()});
+
+  
+  $('#playGame').on('click', ()=>{
+    $('#goBox').addClass('d-flex').show(); 
+    $('#gamePiece').show();
+    $('#nameBox').removeClass('d-flex').hide(); 
+    $('#playGame').prop('disabled', true); 
+  });
+
+  $('#playAgain').on('click', ()=>{
+    $('#gamePiece').css({ left: 0, top: 0 }); 
+    setKittysHouse(); 
+     $("#gamePiece").draggable({ disabled: false });
+  }); 
+
+  $('#resetGame').on('click', ()=>{
+    initializeGame();
+    
+  }
+)
+
+  // drag & drop! 
 // https://www.w3schools.com/howto/howto_js_draggable.asp
 // https://jqueryui.com/draggable/
+
+
 
 $("#gamePiece").draggable(
   {
@@ -16,8 +49,90 @@ $("#gamePiece").draggable(
  $('.gameTargets').droppable({
     accept: '#gamePiece',
     drop: function() {
-        // $( this )
-        console.log("Game piece dropped on target!" + $(this).attr('class'));
+       if($(this).attr('id') === $(gameTargets[winningHouse]).attr('id')) {
+          playerScore++; 
+        }
+        else {
+          playerScore--; 
+        }
+      
+      $('#playerScore').text(playerScore);
+      $('#playAgain').show(); 
+      $("#gamePiece").draggable({ disabled: true });
       }
-   
 });
+
+}); //ends doc ready f/n
+
+//hide all controls from the start
+
+function initializeGame() {
+  
+  // Hide the game piece
+  $('#gamePiece').hide().css('display', 'none');  
+  
+  // Hide the controls except the first one (input name)
+  //this was a struggle! Have to explicit remove the d-flex class
+  $('#inputBox').addClass('d-flex').show(); 
+  $('#nameBox').removeClass('d-flex').hide();  
+  $('#goBox').removeClass('d-flex').hide(); 
+  $('#playAgain').hide();
+
+  setKittysHouse(); 
+
+  playerScore = 0; 
+  $('#playerName').val(''); 
+  $('#enteredPlayerName').val('').attr('placeholder', 'what\'s ur name?'); 
+; 
+  playerName = ''; 
+    
+
+}
+
+function setKittysHouse(){
+  gameTargets = document.getElementsByClassName('gameTargets');
+  winningHouse = Math.floor(Math.random() * gameTargets.length);
+  console.log( $(gameTargets[winningHouse]).attr('id'));
+
+}
+
+
+// greet the user (using jQ)
+function greetPlayer(){
+  // console.log('in greetPlayer() f/n'); //I used these to debug, setting ⛳️ in my code
+  const enteredPlayerName = $('#enteredPlayerName').val(); 
+  // console.log(enteredPlayerName); //another ⛳️
+  // 👇🏻 this checks to make sure a player name is entered!
+  if (enteredPlayerName) {
+    $('#displayPlayerName').prepend(`Get ready to help the kitty, ${enteredPlayerName}`);
+    $('#playerName').val(''); 
+    
+    //👇🏻 acknowledges their name & makes sure no red border 
+    $('#enteredPlayerName')
+      .attr('placeholder', 'Cool 😎') 
+      .removeClass('redBorder');
+
+    $('#nameBox').addClass('d-flex').show();  
+    $('#inputBox').removeClass('d-flex').hide(); 
+   
+  
+  }
+  else if(!enteredPlayerName) {
+    console.log('no player name');
+    //👇🏻 brings attention to no name 
+    //👇🏻 set any DOM attribute https://www.w3schools.com/jquery/html_attr.asp
+    $('#enteredPlayerName')
+      .attr('placeholder', 'ppppllllleeeaaasssee enter ur name 😽')
+      .addClass('redBorder')
+      .focus(); 
+    // https://api.jquery.com/toggleClass/
+    // 👆🏻 did you know you can "chain" methods togehter. Above is really one line of code but split out with each .method() for readability (is that a word?)
+      $('#displayPlayerName').text('');
+      $('#playGame').hide(); 
+
+  
+  }
+}
+
+
+
